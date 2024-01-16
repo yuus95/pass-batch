6358cd583250576c5e1eee860fd6b489c70f0a4c
yushin <kkad45@naver.com>
2024-01-16 22:12:06 +0900

    feat/수업전-알람전송 카카오어뎁터 구현

--- src/main/java/com/fastcampus/pass/job/notification/SendNotificationItemWriter.java
+++ src/main/java/com/fastcampus/pass/job/notification/SendNotificationItemWriter.java
@@ -1,4 +1,18 @@
 package com.fastcampus.pass.job.notification;
 
+import com.fastcampus.pass.adapter.message.KakaoTalkMessageAdapter;
+import com.fastcampus.pass.repository.notification.NotificationRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
 public class SendNotificationItemWriter {
+    private final NotificationRepository notificationRepository;
+    private final KakaoTalkMessageAdapter kakaoTalkMessageAdapter;
+
+    public SendNotificationItemWriter(NotificationRepository notificationRepository, KakaoTalkMessageAdapter kakaoTalkMessageAdapter) {
+        this.notificationRepository = notificationRepository;
+        this.kakaoTalkMessageAdapter = kakaoTalkMessageAdapter;
+    }
 }
