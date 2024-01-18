c743ccc74c0e0b34d0964ab5d906a700cb9e6453
yushin <kkad45@naver.com>
2024-01-18 23:08:30 +0900

    feat/수업전-알람전송 알람전송 배치 기능 구현

--- src/main/java/com/fastcampus/pass/job/notification/SendNotificationBeforeClassJobConfig.java
+++ src/main/java/com/fastcampus/pass/job/notification/SendNotificationBeforeClassJobConfig.java
@@ -5,15 +5,23 @@ import com.fastcampus.pass.repository.booking.BookingStatus;
 import com.fastcampus.pass.repository.notification.NotificationEntity;
 import com.fastcampus.pass.repository.notification.NotificationEvent;
 import com.fastcampus.pass.repository.notification.NotificationModelMapper;
+import com.fastcampus.pass.repository.notification.NotificationRepository;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaCursorItemReader;
 import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
 import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.batch.item.support.builder.SynchronizedItemStreamReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 import javax.persistence.EntityManagerFactory;
 import java.time.LocalDateTime;
@@ -27,12 +35,15 @@ public class SendNotificationBeforeClassJobConfig {
     private final StepBuilderFactory stepBuilderFactory;
     private final EntityManagerFactory entityManagerFactory;
     private final SendNotificationItemWriter sendNotificationItemWriter;
+    private final NotificationRepository notificationRepository;
 
-    public SendNotificationBeforeClassJobConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, EntityManagerFactory entityManagerFactory, SendNotificationItemWriter sendNotificationItemWriter) {
+    public SendNotificationBeforeClassJobConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, EntityManagerFactory entityManagerFactory, SendNotificationItemWriter sendNotificationItemWriter,
+                                                NotificationRepository notificationRepository) {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
         this.entityManagerFactory = entityManagerFactory;
         this.sendNotificationItemWriter = sendNotificationItemWriter;
+        this.notificationRepository = notificationRepository;
     }
 
     @Bean
@@ -49,7 +60,7 @@ public class SendNotificationBeforeClassJobConfig {
                 .<BookingEntity, NotificationEntity>chunk(CHUNK_SIZE)
                 .reader(addNotificationItemReader())
                 .processor(addNotificationItemProcessor())
-//                .writer(addNotificationItemWriter)
+                .writer(addNotificationItemWriter())
                 .build();
     }
 
@@ -70,4 +81,37 @@ public class SendNotificationBeforeClassJobConfig {
         return bookingEntity ->
                 NotificationModelMapper.INSTANCE.toNotification(bookingEntity, NotificationEvent.BEFORE_CLASS);
     }
+
+    @Bean
+    public ItemWriter<NotificationEntity> addNotificationItemWriter() {
+        return new JpaItemWriterBuilder<NotificationEntity>()
+                .entityManagerFactory(entityManagerFactory)
+                .build();
+    }
+
+    @Bean
+    public Step sendNotificationStep() {
+        return this.stepBuilderFactory.get("sendNotificationStep")
+                .<NotificationEntity, NotificationEntity>chunk(CHUNK_SIZE)
+                .reader(sendNotificationItemReader())
+                .writer(sendNotificationItemWriter)
+                .taskExecutor(new SimpleAsyncTaskExecutor()) //가장 간단한 멀티스레드 TaskExecutor를 선언
+                .build();
+    }
+
+    @Bean
+    public ItemReader<? extends NotificationEntity> sendNotificationItemReader() {
+        JpaCursorItemReader<NotificationEntity> itemReader = new JpaCursorItemReaderBuilder<NotificationEntity>()
+                .name("sendNotificationItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                // 이벤트(event)가 수업 전이며, 발송 여부(sent)가 미발송인 알람이 조회 대상이 된다.
+                .queryString("selcet n from NotificationEntity n where n.event = :event and n.sent = :sent")
+                .parameterValues(Map.of("event", NotificationEvent.BEFORE_CLASS, "sent", false))
+                .build();
+
+        return new SynchronizedItemStreamReaderBuilder<NotificationEntity>()
+                .delegate(itemReader)
+                .build();
+    }
+
 }
